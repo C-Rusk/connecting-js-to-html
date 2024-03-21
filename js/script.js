87f2d8a6d740e9477c72144b5c457abc98733cd4
Cassandra Rusk <149620042+C-Rusk@users.noreply.github.com>
2024-03-21 14:18:45 -0500

    Connecting JavaScript to HTML

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//console.log("Hi!");
+var mainTitle = document.querySelector("h1");
+console.log(mainTitle.outerHTML);
+mainTitle.innerText = "Yay, JavaScript!";
+
+var body = document.querySelector("body");
+console.log(body.outerHTML);
+body.style.backgroundColor = "#FEF2F1";
